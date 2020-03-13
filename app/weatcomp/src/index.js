259072af424eef11ec9262b3452f2e55a72fcbfd
Vladimir Meshcheryakov <vlames@pdx.edu>
2020-03-12 22:22:44 -0700

    copied application over

---
+++ app/weatcomp/src/index.js
@@ -0,0 +1,9 @@
+// The file describes code that imports various components
+// to start the app then ReactDOM renders the main component,
+// App to start the application
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
