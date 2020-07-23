86c3b95c2c5764bcb54349272ef68d071d899b8d
Vladimir Meshcheryakov <vlames@pdx.edu>
2020-07-22 23:36:39 -0700

    add the js file to open specific github page to view project repositories

---
+++ js/general.js
@@ -0,0 +1,23 @@
+function show_quizcreator() {
+    window.open( "https://github.com/vlames/quizcreator", "_blank"); 
+}
+
+function show_subops() {
+    window.open( "https://github.com/vlames/subops", "_blank"); 
+}
+
+function show_receiptcreator() {
+    window.open( "https://github.com/vlames/receiptcreator", "_blank"); 
+}
+
+function show_weatcomp() {
+    window.open( "https://github.com/vlames/weatcomp", "_blank"); 
+}
+
+function show_memoverse() {
+    window.open( "https://github.com/vlames/memoverse", "_blank"); 
+}
+
+function show_portlandintro() {
+    window.open( "https://github.com/Sanlinlin/Frontend2020Spring", "_blank"); 
+}
